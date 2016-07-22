1834c3f7cc5a1bbd58653372fb0efbb3c4a31e73
Michael <michael@slapdogaf.dk>
2016-07-22 07:47:35 +0200

    Added missing file

---
+++ ffmpeg-farm-server/Contract/Mediainfo.cs
@@ -0,0 +1,13 @@
+namespace Contract
+{
+    public class Mediainfo
+    {
+        public bool Interlaced { get; set; }
+
+        public double Framerate { get; set; }
+
+        public int Duration { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
