badcee9f6c3c36effd72029a3fd9fe120f40b52e
Bo Bendtsen <bben@dr.dk>
2016-09-29 13:21:39 +0200

    ws dummy logger added. temp todo log to event log

--- ffmpeg-farm-client/WindowsService/Service.cs
+++ ffmpeg-farm-client/WindowsService/Service.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ServiceProcess;
 using System.Threading;
 using FFmpegFarm.WindowsService.Properties;
+using FFmpegFarm.Worker;
 
 namespace FFmpegFarm.WindowsService
 {
@@ -9,9 +11,19 @@ namespace FFmpegFarm.WindowsService
     {
         private IList<Thread> _threads;
         private CancellationTokenSource _cancellationTokenSource;
+        private static ILogger _logger = new DummyLogger();
         public Service()
         {
             InitializeComponent();
+        }
+
+        private class DummyLogger : ILogger
+        {
+            public void Debug(string text) { }
+
+            public void Warn(string text) { }
+
+            public void Exception(Exception exception) { }
         }
 
         protected override void OnStart(string[] args)
@@ -20,7 +32,7 @@ namespace FFmpegFarm.WindowsService
             _cancellationTokenSource = new CancellationTokenSource();
             for (var x = 0; x < Settings.Default.Threads; x++)
             {
-                var thread = new Thread(() => new Worker.Node(Settings.Default.FFmpegPath, Settings.Default.ControllerApi).Run(_cancellationTokenSource.Token));
+                var thread = new Thread(() => new Worker.Node(Settings.Default.FFmpegPath, Settings.Default.ControllerApi, _logger).Run(_cancellationTokenSource.Token));
                 _threads.Add(thread);
                 thread.Start();
             }
